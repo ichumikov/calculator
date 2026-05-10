5510d64cf5df55fb0e81302b49692f9354219d4c
Igor Chumikov <2vbirf@gmail.com>
2026-05-10 20:01:39 +0000

    feat: add Calculator class with JSON input parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
-#include "app.h"
+#include "calculator.h"
+#include <iostream>
+#include <stdexcept>
+
 int main(int argc, char** argv)
 {
-    app::run(argc, argv);
-    return 0;
+if (argc != 2)
+{
+    std::cerr<< "not or many arguments";
+    return 1;
+}
+try
+{
+    Calculator calc(argv[1]);  
+    int result = calc.calculate(); 
+    std::cout << result << "\n";  
+}
+catch (const std::exception& e)
+{
+    std::cerr << "Error: " << e.what() << "\n";
+    return 1;
+}
 }
